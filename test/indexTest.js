eb99b3c5684f3425c6773b82f4cc89aa9e2dba00
rchow1944 <rchow2@buffalo.edu>
2019-02-25 13:49:23 -0500

    Done.

--- test/indexTest.js
+++ test/indexTest.js
@@ -1,6 +1,6 @@
-describe("index.js", () => {
-  it("brunch equals brunch", () => {
-    expect(Object).to.exist
-    expect(Object).to.equal('toast: "avocado", mimosa: "sunburst orange juice", eggs: "bacon deviled", pancakes: "mini skewers"')
-  })
-})
+// describe("index.js", () => {
+//   it("brunch equals brunch", () => {
+//     expect(brunch).to.exist
+//     expect(brunch).to.equal({toast: "avocado", mimosa: "sunburst orange juice", eggs: "bacon deviled", pancakes: "mini skewers"})
+//   })
+// })
